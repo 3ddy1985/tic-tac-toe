f48ffa794fa94fbcc14ea1ad32a618f93fb45d14
Nick <nick.langley@hotmail.com>
2023-01-05 19:52:07 +0000

    added more styling

--- main.js
+++ main.js
@@ -12,27 +12,27 @@ class gameGrid {
         }
     }
     boardCode() {
-        return `<div class="row players">
+        return `<div class="players">
                     <div id="player-1">Player X</div>
                     <div id="player-2">Player O</div>
-                    
-                    <table class="tic-tac">
-                        <tr>
-                            <td id="cell-1">${this.cells[0].cellContent()}</td>
-                            <td id="cell-2">${this.cells[1].cellContent()}</td>
-                            <td id="cell-3">${this.cells[2].cellContent()}</td>
-                        </tr>
-                        <tr>
-                            <td id="cell-4">${this.cells[3].cellContent()}</td>
-                            <td id="cell-5">${this.cells[4].cellContent()}</td>
-                            <td id="cell-6">${this.cells[5].cellContent()}</td>
-                        </tr>
-                        <tr>
-                            <td id="cell-7">${this.cells[6].cellContent()}</td>
-                            <td id="cell-8">${this.cells[7].cellContent()}</td>
-                            <td id="cell-9">${this.cells[8].cellContent()}</td>
-                        </tr>
-                    </table>`
+                </dv>    
+                <table class="tic-tac">
+                    <tr>
+                        <td id="cell-1">${this.cells[0].cellContent()}</td>
+                        <td id="cell-2">${this.cells[1].cellContent()}</td>
+                        <td id="cell-3">${this.cells[2].cellContent()}</td>
+                    </tr>
+                    <tr>
+                        <td id="cell-4">${this.cells[3].cellContent()}</td>
+                        <td id="cell-5">${this.cells[4].cellContent()}</td>
+                        <td id="cell-6">${this.cells[5].cellContent()}</td>
+                    </tr>
+                    <tr>
+                        <td id="cell-7">${this.cells[6].cellContent()}</td>
+                        <td id="cell-8">${this.cells[7].cellContent()}</td>
+                        <td id="cell-9">${this.cells[8].cellContent()}</td>
+                    </tr>
+                </table>`
     }
 }
 
